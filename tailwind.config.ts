5cf0871ad5fa53eab15e56b4e28b1d3ded5629a9
lidong <134201873+litaibai1201@users.noreply.github.com>
2025-05-10 13:58:58 +0800

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.ts
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+      preflight: false,
+    },
+  };
+  
